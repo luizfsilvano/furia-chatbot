efd8997d5277af41d39158ec4124fd2e1c6a5a53
luizfsilvano <luizfsilvano@gmail.com>
2025-04-25 03:02:29 -0300

    feat(chat): implement base structure with domain contracts, service, and controller

--- src/main/java/com/furia/chatbot/infrastructure/tracker/InteractionLoggerImpl.java
+++ src/main/java/com/furia/chatbot/infrastructure/tracker/InteractionLoggerImpl.java
@@ -1,4 +1,31 @@
 package com.furia.chatbot.infrastructure.tracker;
 
-public class InteractionLoggerImpl {
+import com.furia.chatbot.domain.tracker.InteractionLogger;
+import org.springframework.stereotype.Component;
+
+@Component
+public class InteractionLoggerImpl  implements InteractionLogger {
+    @Override
+    public void incrementMessageCount()
+    {
+        // TODO: Count messages
+    }
+
+    @Override
+    public void incrementFallbackUsage()
+    {
+        // TODO: Count Fallbacks
+    }
+
+    @Override
+    public int getTotalMessages() {
+        return 0;
+    }
+
+    @Override
+    public int getFallbackUsage() {
+        return 0;
+    }
+
+
 }
--- src/main/java/com/furia/chatbot/web/controller/ChatController.java
+++ src/main/java/com/furia/chatbot/web/controller/ChatController.java
@@ -1,4 +1,27 @@
 package com.furia.chatbot.web.controller;
 
+
+import com.furia.chatbot.application.service.ChatService;
+import com.furia.chatbot.web.dto.UserMessageRequest;
+import org.springframework.http.ResponseEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/chat")
+@RequiredArgsConstructor
 public class ChatController {
+
+    private final ChatService chatService;
+
+    @PostMapping
+    public ResponseEntity<String> chat(@RequestBody UserMessageRequest request)
+    {
+        String response = chatService.handleUserMessage(request.message());
+        return ResponseEntity.ok(response);
+    }
+
 }
