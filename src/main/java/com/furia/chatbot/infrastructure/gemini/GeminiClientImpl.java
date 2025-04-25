2f37f18a83cf824f024a7bd9e20b2b8131e4f4ed
luizfsilvano <luizfsilvano@gmail.com>
2025-04-25 03:34:21 -0300

    feat(gemini): implement GeminiClient with API call to Gemini 2.0 flash lite model

--- src/main/java/com/furia/chatbot/infrastructure/gemini/GeminiClientImpl.java
+++ src/main/java/com/furia/chatbot/infrastructure/gemini/GeminiClientImpl.java
@@ -1,15 +1,52 @@
 package com.furia.chatbot.infrastructure.gemini;
 
 import com.furia.chatbot.domain.gateway.GeminiGateway;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.*;
 
 @Component
+@RequiredArgsConstructor
 public class GeminiClientImpl implements GeminiGateway {
+
+    @Value("${gemini.api.key}")
+    private String API_KEY;
+    private final RestTemplate restTemplate = new RestTemplate();
+
     @Override
     public String getResponse(String userMessage)
     {
-        // TODO: Integrate with Gemini API
-        return null;
-    }
+        String url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash-lite:generateContent?key=%s"
+                .formatted(API_KEY);
 
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        String requestBody = """
+                {
+                    "contents": [
+                        {
+                            "parts": [
+                                {
+                                    "text": "%s"
+                                }
+                            ]
+                        }
+                    ]
+                }
+                """.formatted(userMessage);
+
+        HttpEntity<String> entity = new HttpEntity<>(requestBody, headers);
+
+        ResponseEntity<String> response = restTemplate.postForEntity(url, entity, String.class);
+
+        if (response.getStatusCode().is2xxSuccessful()) {
+            return response.getBody();
+        } else {
+            throw new RuntimeException("Failed to call Gemini API: " + response.getStatusCode() + " " + response.getBody());
+        }
+    }
 }
